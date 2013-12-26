7061e64bfebeb9df1bd0c6d51947831ab9bf5a90
ice <hbcomeon2012@126.com>
2013-12-26 12:44:11 +0800

    o

---
+++ gen/cn/qicainiao/guoneilvyou_qi1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.qicainiao.guoneilvyou_qi1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cn/waps/demo188/MyAdView.java
@@ -0,0 +1,21 @@
+package cn.waps.demo188;
+
+import android.os.Bundle;
+
+import com.qcn.wzlz.OffersWebView;
+
+public class MyAdView extends OffersWebView {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.waps.OffersWebView#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+
+	}
+
+}
